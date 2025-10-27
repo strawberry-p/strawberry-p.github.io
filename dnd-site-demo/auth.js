543008956d97e008ea4a8398aadd37a4904629ca
strawberry-p <212942494+strawberry-p@users.noreply.github.com>
2025-10-27 06:23:08 +0100

    Update auth.js
    Try again

--- dnd-site-demo/auth.js
+++ dnd-site-demo/auth.js
@@ -17,6 +17,7 @@ function ret_auth_values() {
 function kick_url(){
     let url = window.location.pathname
     let wl = window.location //"in" doesn't work in JS apparently
+    return "no_redirect"
     if (wl.protocol.includes("file") || true) {return "file"} //ts local testing is pmo.
     if (authed && (url.includes("login") || url.includes("register"))) {
         console.log("redirect to "+wl.protocol+wl.hostname+"/campaigns.html")
@@ -32,3 +33,4 @@ function kick_url(){
 placeholder()
 
 kick_url()
+
